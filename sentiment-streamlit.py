c5682cb24a3468b8d95bc545a9884b513b02fa6b
Manas9090 <150647362+Manas9090@users.noreply.github.com>
2025-03-22 16:16:41 +0530

    Update sentiment-streamlit.py

--- sentiment-streamlit.py
+++ sentiment-streamlit.py
@@ -4,8 +4,9 @@ from langsmith import Client, trace
 
 # Load API keys securely
 openai.api_key = st.secrets["openai"]["api_key"]
+client = Client(api_key=st.secrets["LANGSMITH"]["API_KEY"])
+#client = Client(api_key=st.secrets["LANGSMITH_API_KEY"])
 client = Client(api_key=st.secrets["LANGSMITH_API_KEY"])
-#client = Client(LANGSMITH_API_KEY1=st.secrets["LANGSMITH_API_KEY"])
 
 # Add tracing decorator to monitor the function
 @trace(name="Sentiment Analysis", metadata={"app": "Streamlit Sentiment Analysis"})
